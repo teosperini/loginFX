feffc6aa68e14904ea051814f25dfae91d5438c5
teosperini <matteosperini@gmail.com>
2024-01-28 20:02:45 +0100

    miao miao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example.loginfx {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
+
 
     opens com.example.loginfx to javafx.fxml;
     exports com.example.loginfx;
